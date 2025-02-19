ae004e16f4f1998eb6435c34ee8465f05acc8848
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-19 16:13:02 +0100

    refactor: remove unused page routes and serve static files directly from client dist

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv')
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
@@ -7,8 +8,7 @@ dotenv.config();
 app.use(cors()).use(express.json({limit: '50mb'})).use(express.urlencoded({ extended: true }));
 
 const apiRouter = require("./src/routes/api/api.js");
-const routes = require("./src/routes/pages/routes.js");
 app.use("/api", apiRouter);
-app.use('/', routes);
+app.use(express.static('client/dist'));
 
 module.exports = app.set('port', process.env.PORT ?? 3000).set('host', process.env.HOST);
--- src/routes/pages/routes.js
+++
@@ -1,8 +0,0 @@
-const homeRouter = require('./homeRouter');
-const express = require('express');
-
-const router = express.Router();
-
-router.use('', homeRouter);
-
-module.exports = router;
