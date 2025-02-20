51bf2a02c793dbf3b1c94a12e0e3e7ca461b7c54
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-20 12:30:02 +0100

    feat: enhance routing with authentication checks and add new routes for register and logout

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { useAuthStore } from './stores/auth-store';
 
-export default createRouter({
+const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         {
@@ -11,5 +12,43 @@ export default createRouter({
             path: '/login',
             component: () => import('./pages/LoginPage.vue')
         },
-    ]
-})
+        {
+            path: '/register',
+            component: () => import('./pages/RegisterPage.vue')
+        },
+        {
+            path: '/logout',
+            redirect: '/login'
+        },
+        {
+            path: '/dashboard',
+            component: () => import('./pages/DashboardPage.vue'),
+            meta: { requiresAuth: true },
+        },
+    ],
+});
+
+router.beforeEach((to, _, next) => {
+    const authStore = useAuthStore();
+    const isLoggedIn = authStore.getToken ?? false;
+
+    console.log('isLoggedIn:', isLoggedIn);
+
+    if (to.meta.requiresAuth && !isLoggedIn) {
+        return next('/login');
+    } else if (isLoggedIn) {
+        if (to.path === '/login' || to.path === '/register')
+            return next('/dashboard');
+        else if (to.path === '/logout') {
+            if (authStore.logout())
+                return next('/login');
+            else
+                console.error('Logout failed');
+                return next('/dashboard');
+        }
+    }
+
+    return next();
+});
+
+export default router;
