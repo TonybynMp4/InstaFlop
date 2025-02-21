4dcbb95153265c147b3c1e9e006c1dccecc092b9
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-21 20:36:10 +0100

    feat: add countdown timer and update HomePage layout

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,44 +1,43 @@
 <script setup lang="ts">
-    import baseURL from '../baseUrl';
-    import { onBeforeMount, ref } from 'vue';
-    import type { User } from '../types';
+    import { ref } from 'vue';
+    import { useRouter } from 'vue-router';
 
-    interface ExtendedUser extends User {
-        id: number;
-        createdRelativeTime: number;
-    }
+    const router = useRouter();
 
-    const users = ref<ExtendedUser[]>([]);
+    //calculer le temps restant avant le 2 avril 2025 14h
+    const timeLeft = ref('');
 
-    onBeforeMount(async () => {
-        const response = await fetch(`${baseURL}/api/user/getUsers`);
-        const data = await response.json();
+    const calculateTimeLeft = () => {
+        const difference = +new Date('2025-04-02 14:00') - +new Date();
 
-        if (!response.ok) {
-            console.error(data);
+        if (difference <= 0) {
+            timeLeft.value = 'C\'est toujours là? Oh la sauce..';
             return;
         }
 
-        users.value = data.map((user: any) => {
-            const diffInHours = Math.floor((new Date(user.created_at).getTime() - Date.now()) / (1000 * 60 * 60))
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((difference / 1000 / 60) % 60);
+        const seconds = Math.floor((difference / 1000) % 60);
+        timeLeft.value = `${days} jours, ${hours} heures, ${minutes} minutes, ${seconds} secondes`;
+    };
 
-            return {
-                id: user.id,
-                username: user.username,
-                createdRelativeTime: diffInHours
-            };
-        });
-    });
+    calculateTimeLeft();
+    setInterval(calculateTimeLeft, 1000);
+
 </script>
 
 <template>
     <main>
-        <h1>Home Page !</h1>
-        <ul id="user-list">
-            <li v-for="user in users" :key="user.id">
-                <p>{{ user.username }} Created At: {{ new Intl.RelativeTimeFormat('fr-FR').format(user.createdRelativeTime, 'hours') }}</p>
-            </li>
-        </ul>
+        <h1>Bienvenue sur le pire réseau social du monde</h1>
+
+        <p>
+            WIP: ce projet est encore en cours de développement!
+        </p>
+        <p style="font-size: 1.5rem; font-weight: bold;">
+            Temps restant avant qu'on flop le cours de Node.js:
+        </p>
+        <time style="font-size: 2rem; font-weight: bold;">{{ timeLeft }}</time>
     </main>
 </template>
 
