f5bdca33be78d4c3c60ff79b32f35cf26d11d784
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-21 21:51:47 +0100

    fix: add CORS middleware to API function

--- src/netlify/functions/api.js
+++ src/netlify/functions/api.js
@@ -3,8 +3,9 @@ import serverless from 'serverless-http';
 export async function handler(event, context) {
     const express = require('express');
     const app = express();
+    const cors = require('cors');
 
-    app.use(express.json({limit: '50mb'})).use(express.urlencoded({ extended: true }));
+    app.use(cors()).use(express.json({limit: '50mb'})).use(express.urlencoded({ extended: true }));
 
     const apiRouter = require("../../routes/api/api.js");
     app.use("/api", apiRouter);
