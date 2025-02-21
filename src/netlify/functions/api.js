92cb908615b2cd010352ea0d664d1b2b5b50c202
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-21 21:49:37 +0100

    refactor: completely isolate node & netlify

--- src/netlify/functions/api.js
+++ src/netlify/functions/api.js
@@ -1,6 +1,13 @@
-import app from '../../../app';
 import serverless from 'serverless-http';
 
 export async function handler(event, context) {
+    const express = require('express');
+    const app = express();
+
+    app.use(express.json({limit: '50mb'})).use(express.urlencoded({ extended: true }));
+
+    const apiRouter = require("../../routes/api/api.js");
+    app.use("/api", apiRouter);
+
     return serverless(app)(event, context);
 }
