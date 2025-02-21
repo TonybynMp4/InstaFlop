4555c8e397e3639c4be26a22f124a8892aafc29f
Antony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-21 23:13:25 +0100

    chore: netlify server functions (serverless express api) (#1)

---
+++ src/netlify/functions/api.js
@@ -0,0 +1,10 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+app.use(express.json({ limit: '50mb' })).use(express.urlencoded({ extended: true }));
+
+const apiRouter = require("../../routes/api/api.js");
+app.use("/api", apiRouter);
+
+module.exports.handler = serverless(app);
