ed7c4608cde7bda90a10aa6e14f332b3cf38ec89
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-21 20:43:47 +0100

    feat: (still testing) configure Netlify functions and integrate serverless-http for deployment

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const serverless = require('serverless-http');
 
+module.exports.handler = serverless(app);
+
+
+/* good ol' Node.js way
 const port = app.get('port');
 app.listen(port, () => {
     console.log(`Server listening on ${app.get('host')} ${port}`);
 });
+*/
