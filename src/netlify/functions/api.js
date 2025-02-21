d40ce0822ee0d631f3be2f1b81505a85626dd5e5
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-21 22:00:58 +0100

    test endpoints

--- src/netlify/functions/api.js
+++ src/netlify/functions/api.js
@@ -1,14 +1,23 @@
 import serverless from 'serverless-http';
 
-export async function handler(event, context) {
-    const express = require('express');
-    const app = express();
-    const cors = require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-    app.use(cors()).use(express.json({limit: '50mb'})).use(express.urlencoded({ extended: true }));
+app.use(cors()).use(express.json({limit: '50mb'})).use(express.urlencoded({ extended: true }));
 
-    const apiRouter = require("../../routes/api/api.js");
-    app.use("/api", apiRouter);
+const apiRouter = require("../../routes/api/api.js");
+app.use("/api", apiRouter);
+app.get('/api', (req, res) => {
+    res.send('Hello World!')
+});
+app.get('/api/test', (req, res) => {
+    res.send('Test World!')
+});
 
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+    await handler(event, context);
     return serverless(app)(event, context);
 }
