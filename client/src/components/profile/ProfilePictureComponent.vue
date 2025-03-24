4de614597b7c32ca965e32c997099b80ce20a703
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-03-25 00:22:53 +0100

    feat: add ProfilePictureComponent and UsernameComponent for user profile display

---
+++ client/src/components/profile/ProfilePictureComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+	import { defineProps } from 'vue';
+	interface ProfilePictureComponent {
+		src: string;
+	}
+
+	const props = defineProps<ProfilePictureComponent>();
+	const { src } = props;
+</script>
+
+<template>
+	<img class="profile_picture" :src="src" alt="Profile Picture" />
+</template>
+
+<style scoped>
+	.profile_picture {
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+		object-fit: cover;
+		border: 1px solid #ffffff;
+	}
+</style>
---
+++ client/src/components/profile/UsernameComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+	import { defineProps } from 'vue';
+	import type { UsernameComponent } from '@/types';
+
+	const props = defineProps<UsernameComponent>();
+	const { username } = props;
+</script>
+
+<template>
+	<router-link :to="`/profile/${username}`" class="username">
+		@{{ username }}
+	</router-link>
+</template>
+
+<style scoped>
+	.username {
+		text-decoration: none;
+		color: var(--primary);
+		font-weight: bold;
+	}
+	.username:hover {
+		text-decoration: underline;
+		color: var(--secondary);
+	}
+</style>
