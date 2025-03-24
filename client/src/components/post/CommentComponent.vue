a314d4e7884aa2bb7475b2fdc6b79236c88f4047
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-03-25 00:22:59 +0100

    feat: add CommentComponent for displaying user comments in posts

---
+++ client/src/components/post/CommentComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+	import { defineProps } from 'vue';
+	import type { CommentComponent } from '@/types';
+	import Username from '@/components/profile/UsernameComponent.vue';
+
+	const props = defineProps<CommentComponent>();
+
+	const { username } = props;
+</script>
+
+<template>
+	<article class="post_comment">
+		<p>
+			<Username :username="username" />
+			{{ content }}
+		</p>
+	</article>
+</template>
+
+<style scoped>
+	.post_comment {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+        width: 100%;
+        border-bottom: 1px solid #bbb;
+        padding-block: 0.5rem;
+        padding-inline: 0.5rem;
+    }
+
+	.post_comment p {
+		margin: 0;
+		overflow: hidden;
+	}
+</style>
